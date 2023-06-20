92e45831ebeba7250bc5bd78cf081dbdf7448f9d
DevChild <vhffptwkd@naver.com>
2023-06-20 19:24:12 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import '@picocss/pico/css/pico.min.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import '@picocss/pico/css/pico.min.css';
+
+const Home = () => {
+  return (
+    <div className='container'>
+      TEST
+    </div>
+    
+  );
+}
+
+
+export default Home;
