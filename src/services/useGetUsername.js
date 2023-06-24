0bf70a2185d3212049ceff602ae2df0fca83d45b
DiligentP <qkrwjdgus0603@gmail.com>
2023-06-24 21:41:31 +0900

    자잘 한 오류 수정

--- src/services/useGetUsername.js
+++ src/services/useGetUsername.js
@@ -3,14 +3,14 @@ import axios from "axios";
 import {server} from "./apiServer";
 
 const useGetUsername = (username) => {
-    const [ckUserUsername, setckUserUsername] = useState(false);
+    const [ckUsername, setCkUsername] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
-            if(username.length <= 3) return !ckUserUsername
+            if(username.length <= 3) return !ckUsername
             try {
                 const response = await axios.get(`${server}/users?username=${username}`);
-                setckUserUsername(JSON.parse(response.data.body));
+                setCkUsername(JSON.parse(response.data.body));
             } catch (error) {
                 // API 호출에 실패한 경우에 대한 예외 처리를 여기에 작성하세요.
                 console.error("Failed to fetch:", error);
@@ -20,7 +20,7 @@ const useGetUsername = (username) => {
         fetchData();
     }, [username]);
 
-    return ckUserUsername;
+    return ckUsername;
 };
 
 export default useGetUsername;
